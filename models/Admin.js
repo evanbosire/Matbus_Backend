ea12bcb98717dfb4721f3214f06ada6feeba207c
Brian Bosire Evans <evanbosire422@gmail.com>
2025-05-11 18:30:06 +0300

    made name optional in the schema

--- models/Admin.js
+++ models/Admin.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: false },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: { type: String, required: true },
